e0e8d925ac7a1712b1597107a3d225735f8d6d44
Claire Lee <jclairelee2@gmail.com>
2026-04-01 18:09:03 -0700

    Initialize the project

---
+++ A2_iOS_Claire_100882058/A2_iOS_Claire_100882058App.swift
@@ -0,0 +1,17 @@
+//
+//  A2_iOS_Claire_100882058App.swift
+//  A2_iOS_Claire_100882058
+//
+//  Created by Claire Lee on 2026-04-01.
+//
+
+import SwiftUI
+
+@main
+struct A2_iOS_Claire_100882058App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A2_iOS_Claire_100882058/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A2_iOS_Claire_100882058
+//
+//  Created by Claire Lee on 2026-04-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
