7b4df4cba19cd23dbcdf19e798d73dfd12056333
Claire Lee <jclairelee2@gmail.com>
2026-04-09 11:26:10 -0700

    Set up TabView and connect HomeView as main screen

--- A2_iOS_Claire_100882058/ContentView.swift
+++ A2_iOS_Claire_100882058/ContentView.swift
@@ -1,24 +1,13 @@
-//
-//  ContentView.swift
-//  A2_iOS_Claire_100882058
-//
-//  Created by Claire Lee on 2026-04-01.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
